9a3115c74b8dd0642d2a69371d18dae88f0c5659
Ruei <13717820+JuiChao@users.noreply.github.com>
2026-04-21 10:11:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # Delete somefeeds
-rm -rf ./openwrt/feeds/luci/applications/luci-app-passwall 
-rm -rf ./openwrt/feeds/luci/applications/luci-app-passwall2
+# rm -rf ./openwrt/feeds/luci/applications/luci-app-passwall 
+# rm -rf ./openwrt/feeds/luci/applications/luci-app-passwall2
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
